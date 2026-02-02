41e1bef3baa43c2b9e9d0c4692587ce7aa8ea65e
Cuvillier--Couvreur Béryl <beryl.cuvillier@gmail.com>
2026-02-02 14:28:57 +0100

    tp 7 fini

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from models.categorie import *
 from models.produit import *
+from models.inventaire import *
 from datetime import date
 
 """"
@@ -24,7 +25,7 @@ try:
 except ValueError as e:
     print(e)
 """
-
+"""
 clavier = ProduitElectronique("KB-001", "Clavier RGB", 79.99, 15, 24, 0.5)
 fromage = ProduitAlimentaire("ALI-001", "Comté", 12.99, 50, "2025-06-15")
 
@@ -35,3 +36,32 @@ clavier.afficher_details()  # Garantie: 24 mois, Poids: 0.5kg
 fromage.afficher_details()  # Péremption: 2025-06-15
 
 print(fromage.est_perime())  # False (ou True selon la date)
+"""
+"""
+inv = Inventaire()
+
+inv.ajouter(ProduitElectronique("KB-001", "Clavier", 79.99, 15, 24, 0.5))
+inv.ajouter(ProduitElectronique("SC-001", "Écran", 299.99, 5, 36, 5.0))
+inv.ajouter(ProduitAlimentaire("ALI-001", "Comté", 12.99, 50, "2025-06-15"))
+inv.ajouter(ProduitAlimentaire("ALI-002", "Lait", 1.50, 100, "2024-01-01"))  # Périmé
+
+print(inv.total_frais_livraison())  # 61.0 (11 + 20 + 15 + 15)
+print(len(inv.lister_par_type(ProduitElectronique)))  # 2
+print(len(inv.produits_perimes()))  # 2
+"""
+
+
+p1 = ProduitElectronique("KB-001", "Clavier", 79.99, 8)
+p2 = ProduitAlimentaire("RA-909", "Tomates", 6.99, 10)
+p3 = ProductFactory.create(ProduitElectronique, {"ref": "MS-001", "nom": "Souris", "prix": 49.99, "stock": 5})
+p4 = ProductFactory.create(ProduitAlimentaire, {"ref": "FR-001", "nom": "Comté", "prix": 21.99, "stock": 10})
+print(p1)                   # Clavier (KB-001) - 79.99€ HT
+print(repr(p2))             # Produit('KB-001', 'Clavier', 79.99)
+
+print(p1 < p2)              # False
+print(p1 == p2)             # False
+
+print(Produit.valider_prix(-30))    # False
+print(Produit.valider_prix(49.99))  # True             
+print(repr(p3))
+print(repr(p4))
